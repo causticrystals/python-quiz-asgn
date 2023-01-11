fe266101b574f1e8ee78be860ddc5fae6675a11d
causticrystals <peanutpc173@gmail.com>
2023-01-11 12:46:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,51 @@
 score = 0
 
 # Question 1
-answer1 = input("Who is your teacher? ")
-if answer1 == "Mr. V" or answer1 == "Mr. Veldkamp":
+print("1. Question")
+answer1 = input("Q1 Answer: ").toLower()
+if answer1 == "placeholder":
     print("Correct")
     score += 1
 else:
     print("Incorrect")
-
 
 # Question 2
-answer2 = input("What is 2 + 3")
-if answer2 == "5":
+print("2. Question")
+answer2 = input("Q2 Answer: ").toLower()
+if answer2 == "placeholder":
     print("Correct")
     score += 1
 else:
     print("Incorrect")
+
+# Question 3
+print("3. Question")
+answer3 = input("Q3 Answer: ").toLower()
+if answer3 == "placeholder":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+# Question 4
+print("4. Question")
+answer4 = input("Q4 Answer: ").toLower()
+if answer4 == "placeholder":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+# Output Result of Quiz
+print("Your score is: " + str(score) + " / 4 " + str((score / 4) * 100) + "%")
+
+if score == 4:
+    print("Awesome!")
+elif score == 3:
+    print("Good Job")
+elif score == 2:
+    print("Okay")
+elif score == 1:
+    print("Try Again?")
+else:
+    print(":-/")
