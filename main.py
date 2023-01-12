d577a5c8023bec337f3fb599c05b0b84381e7339
causticrystals <peanutpc173@gmail.com>
2023-01-12 12:26:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,36 +6,36 @@ print("Welcome to the quiz")
 score = 0
 
 # Question 1
-print("\n1. Question")
-answer1 = input("Q1 Answer: ").toLower()
-if answer1 == "placeholder":
+print("\n1. Who are the developers of the Half-Life series?")
+answer1 = input("Q1 Answer: ").lower()
+if answer1 == "valve" or answer1 == "captivation digital laboratories":
     print("Correct")
     score += 1
 else:
     print("Incorrect")
 
 # Question 2
-print("\n2. Question")
-answer2 = input("Q2 Answer: ").toLower()
-if answer2 == "placeholder":
+print("\n2. Which sandbox game was initially made as a mod for Half-Life 2?")
+answer2 = input("Q2 Answer: ").lower()
+if answer2 == "gmod" or answer2 == "garry's mod":
     print("Correct")
     score += 1
 else:
     print("Incorrect")
 
 # Question 3
-print("\n3. Question")
-answer3 = input("Q3 Answer: ").toLower()
-if answer3 == "placeholder":
+print("\n3. Which engine was Half-Life developed on?")
+answer3 = input("Q3 Answer: ").lower()
+if answer3 == "goldsrc" or answer3 == "gold source":
     print("Correct")
     score += 1
 else:
     print("Incorrect")
 
 # Question 4
-print("\n4. Question")
-answer4 = input("Q4 Answer: ").toLower()
-if answer4 == "placeholder":
+print("\n4. Which first person shooter was released by Valve in 1998?")
+answer4 = input("Q4 Answer: ").lower()
+if answer4 == "half life" or answer4 == "half-life":
     print("Correct")
     score += 1
 else:
